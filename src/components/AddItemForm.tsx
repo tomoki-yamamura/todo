0733f20392b4458b9c7882cfce71de536b6b4658
tomoki-yamamura <progtab17@gmail.com>
2024-06-25 02:22:07 +0800

    submit logic

--- src/components/AddItemForm.tsx
+++ src/components/AddItemForm.tsx
@@ -1,25 +1,43 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import Button from "./Button";
 import { TItem } from "./ItemLinst";
 
-export default function AddItemForm({setItems}: {
+export default function AddItemForm({ setItems }: {
   setItems: React.Dispatch<React.SetStateAction<TItem[]>>;
 }) {
   const [itemText, setItemText] = useState("")
-   
-  return (
-    <form onSubmit={(e) => {
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
       e.preventDefault()
+
+      if (!itemText) {
+        alert("Item can not be empty");
+        inputRef.current?.focus();
+        return;
+      }
       const newItem = {
-        id: 4,
+        id: new Date().getTime(),
         name: itemText,
         packed: false
       }
 
       setItems(prev => [...prev, newItem])
-    }}>
+      setItemText("")
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
       <h2>Add an item</h2>
-      <input value={itemText} onChange={(e) => setItemText(e.target.value)}/>
+      <input
+        value={itemText}
+        ref={inputRef}
+        onChange={(e) => {
+          setItemText(e.target.value)
+        }}
+        autoFocus={true}
+      />
+
       <Button type="">Add to list</Button>
     </form>
   )
