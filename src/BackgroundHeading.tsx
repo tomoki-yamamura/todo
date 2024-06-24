81480e6fa190138a69feb574af9104fcb4b20e33
tomoki-yamamura <progtab17@gmail.com>
2024-06-24 13:13:52 +0800

    first css

---
+++ src/BackgroundHeading.tsx
@@ -0,0 +1,5 @@
+export default function BackgroundHeading() {
+  return (
+    <h1>ToDo List</h1>
+  )
+}
---
+++ src/Footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <footer>
+      <small>&copy; 2025 Copyright by TM</small>
+      <p>Version 1.5</p>
+    </footer>
+  )
+}
